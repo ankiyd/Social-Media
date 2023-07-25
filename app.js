854572df10ec576b9c775ff7316df72bd66699b9
ankiyd <136189032+ankiyd@users.noreply.github.com>
2023-07-25 21:40:58 +0530

    Update app.js

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ app.use(express.json())
 app.use("/api/user", router);
 app.use("/api/blog", blogRouter);
 
-mongoose.connect('mongodb+srv://ankitayadav207:q0H2hX5tUcN8ayVl@cluster0.ghzwcp3.mongodb.net/Blog?retryWrites=true&w=majority')
+mongoose.connect('mongodb+srv://username:password@cluster0.ghzwcp3.mongodb.net/Blog?retryWrites=true&w=majority')
 .then(() => app.listen(3000))
 .then(() => console.log("Connected"))
 .catch((err) => console.log(err));
